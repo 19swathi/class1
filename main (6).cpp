3029fde3252ca13df4a52b4c80ea36e23be0a729
19swathi <117570575+19swathi@users.noreply.github.com>
2024-02-06 01:21:51 -0500

    Two-Dimensional String Array Access.cpp

--- main (6).cpp
+++ main (6).cpp
@@ -1,10 +1,10 @@
-#include<iostream>
-using namespace std;
-int main()
+#include<iostream>  // Include the input-output stream header for standard I/O operations.
+
+using namespace std;  // Use the standard namespace to simplify access to standard library components.
+
+int main()  // The main function, the entry point of the program.
 {
+    string letters[2][4] = {{"A", "B", "C", "D"}, {"E", "F", "G", "H"}};  // Declare a two-dimensional string array named 'letters' with 2 rows and 4 columns, and initialize it with letters.
 
-
-string letters[2][4] = {{ "A", "B", "C", "D" },{ "E", "F", "G", "H" }};
-cout << letters[0][2];
-
+    cout << letters[0][2];  // Output the element at the intersection of the first row and third column of the 'letters' array.
 }
