c86fffec55805e88d15e622e6fed0ee4f5907183
19swathi <117570575+19swathi@users.noreply.github.com>
2024-02-06 01:18:48 -0500

    Size of Operator Demonstration.cpp

--- main (4).cpp
+++ main (4).cpp
@@ -1,9 +1,12 @@
-#include<iostream>
-using namespace std;
-int main()
-{
+#include<iostream>  // Include the input-output stream header for standard I/O operations.
 
-int mynumbers[5] = {10,20,30,40,50};
-cout << sizeof(mynumbers)<<endl;
-cout << sizeof(int)<<endl;
+using namespace std;  // Use the standard namespace to simplify access to standard library components.
+
+int main()  // The main function, the entry point of the program.
+{
+    int mynumbers[5] = {10, 20, 30, 40, 50};  // Declare an integer array named 'mynumbers' with a size of 5 and initialize it with values.
+
+    cout << sizeof(mynumbers) << endl;  // Output the size (in bytes) of the 'mynumbers' array using the 'sizeof' operator, followed by a newline.
+
+    cout << sizeof(int) << endl;  // Output the size (in bytes) of an integer using the 'sizeof' operator, followed by a newline.
 }
